330ce88eacc9f80e9e2b38289270c8e76c1dc991
AlexisMtr <a.martinier@gmail.com>
2016-04-13 17:48:42 +0200

    Class d'eception CommunicationException

---
+++ ProjetFramework/src/framework/Communication/Exception/CommunicationException.java
@@ -0,0 +1,11 @@
+package framework.Communication.Exception;
+
+public class CommunicationException extends Exception {
+
+	private static final long serialVersionUID = -7579178056698687365L;
+	
+	public CommunicationException(){ super(); }
+	
+	public CommunicationException(String message) { super(message); }
+
+}
