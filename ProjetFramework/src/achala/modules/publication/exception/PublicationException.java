d216ab9439a2d16888c0b3ef89a777862bd6d305
Luc <Luc@Scrubs>
2016-04-15 14:57:26 +0200

    Création PublicationException

---
+++ ProjetFramework/src/achala/modules/publication/exception/PublicationException.java
@@ -0,0 +1,29 @@
+package achala.modules.publication.exception;
+
+public class PublicationException extends Exception {
+
+	public PublicationException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PublicationException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PublicationException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PublicationException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PublicationException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
