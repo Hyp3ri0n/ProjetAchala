ae9d7ad7bfa4b7efc827adcbea8bbf566825f4c4
Hyp3ri0n <aurelien2405@gmail.com>
2016-04-27 16:17:56 +0200

    [Publication]modif insert

--- ProjetFramework/src/achala/modules/publication/metier/Article.java
+++ ProjetFramework/src/achala/modules/publication/metier/Article.java
@@ -52,7 +52,7 @@ public class Article {
 	public void creer() {
 		//Mise à jour des données BD + Context
 		try {
-			ManagerDAO.getBd().request(ManagerDAO.getDAOArticle().insert(this.id, this.date, this.titre, this.contenu, this.auteur));
+			ManagerDAO.getBd().request(ManagerDAO.getDAOArticle().insert(this.id, this.date, this.titre, this.auteur, this.contenu));
 			ManagerApp.Instance().getListArticles().add(this);
 		} catch(Exception e) {
 			e.getMessage();
