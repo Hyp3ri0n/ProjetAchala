0a436dbf48611a58427d96dcda86efde7aae8508
Hyp3ri0n <aurelien2405@gmail.com>
2016-04-28 11:47:00 +0200

    [Framework]Version 7.0

--- ProjetFramework/src/achala/communication/apptest/Client1.java
+++ ProjetFramework/src/achala/communication/apptest/Client1.java
@@ -26,14 +26,14 @@
 //
 //			_Utilisateur alexis = new Utilisateur("Martinier", "Alexis");
 //			
-//			_Server srv = (_Server)Naming.lookup("rmi://130.190.29.31/srv");
+//			_Server srv = (_Server)Naming.lookup("rmi://192.168.43.84/srv");
 //			alexis.connect(srv);
 //			//srv.connect(alexis);
 //			
 //			System.out.println("Start ?");
 //			read.next();			
 //			
-//			_Shared corres = srv.getSharedZone(alexis, srv.getUtilisateur("Ortiz", "Luc"));
+//			_Shared corres = srv.getSharedZone(alexis, "test");
 //			
 //			System.out.println("Votre message : ");
 //			String message = read.next();
