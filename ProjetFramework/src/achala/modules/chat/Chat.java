b24371d0db39cf251c593b5d1065fe67bb3fe215
AlexisMtr <a.martinier@gmail.com>
2016-04-29 08:31:37 +0200

    Commit Cass Chat pour test

--- ProjetFramework/src/achala/modules/chat/Chat.java
+++ ProjetFramework/src/achala/modules/chat/Chat.java
@@ -1,9 +1,9 @@
 package achala.modules.chat;
 
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.List;
-
-import javax.swing.JPanel;
 
 import achala.communication.Message;
 import achala.communication._RemotableObject;
@@ -23,59 +23,56 @@ public class Chat {
 	private List<_Utilisateur> others;
 	private _Server server;
 	private _Shared shared;
-	private ListenerThread listenerThread;
+	private ListenerThread listener;
 	private NotificationThread notifs;
-	
+
 	/**
 	 * Constructeur d'un chat entre utilisateurs u1 et u2 sur le serveur
-	 * @require connected : u1 & u2 connecte
-	 * @param ipSrv String : ip du serveur
-	 * @param current _Utilisateur : utilisateur souhaitant communiquer avec u2
-	 * @param other List<_Utilisateur> : utilisateur a contacter
+	 * 
+	 * @param srv
+	 *            _Server : serveur de communication
+	 * @param curent
+	 *            _Utilisateur : utilisateur souhaitant communiquer avec u2
+	 * @param others
+	 *            List<_Utilisateur> : utilisateur a contacter
 	 */
-	public Chat(String ipSrv, _Utilisateur current, List<_Utilisateur> others, String chatName) {
-		
-		try
-		{
-			this.setServer(Server.getServer(ipSrv));
-			this.setCurrent(current);
-			this.setOther(others);
-			this.getOther().add(this.getCurrent());
-			
-			_Shared correspondance = this.getServer().getSharedZone(this.getCurrent(), chatName);
-			this.setShared(correspondance);
-			this.getShared().addUsers(getOther());
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Constructeur d'un chat entre utilisateurs u1 et u2 sur le serveur
-	 * @param srv _Server : serveur de communication
-	 * @param curent _Utilisateur : utilisateur souhaitant communiquer avec u2
-	 * @param other List<_Utilisateur> : utilisateur a contacter
-	 */
-	public Chat(_Server srv, _Utilisateur curent, List<_Utilisateur> other, String chatName) {
-		
-		try
-		{
+	public Chat(_Server srv, _Utilisateur curent, List<_Utilisateur> others, String chatName) {
+
+		try {
 			this.setServer(srv);
 			this.setCurrent(curent);
-			this.setOther(other);
-			
+			this.setOthers(others);
+
 			_Shared correspondance = this.getServer().getSharedZone(this.getCurrent(), chatName);
-			correspondance.addUsers(other);
+			correspondance.addUsers(others);
 			this.setShared(correspondance);
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	/**
+	 * Constructeur d'un chat entre utilisateurs u1 et u2 sur le serveur
+	 * 
+	 * @require connected : u1 & u2 connecte
+	 * @param ipSrv
+	 *            String : ip du serveur
+	 * @param current
+	 *            _Utilisateur : utilisateur souhaitant communiquer avec u2
+	 * @param other
+	 *            List<_Utilisateur> : utilisateur a contacter
+	 * @throws NotBoundException
+	 *             leve une exception en cas d'impossibilite de bind
+	 * @throws RemoteException
+	 *             leve une exception en cas d'echec de communication
+	 * @throws MalformedURLException
+	 *             leve une excpetion en cas d'URL mal formee
+	 */
+	public Chat(String ipSrv, _Utilisateur current, List<_Utilisateur> others, String chatName)
+			throws MalformedURLException, RemoteException, NotBoundException {
+		this(Server.getServer(ipSrv), current, others, chatName);
+	}
+
 	public _Utilisateur getCurrent() {
 		return current;
 	}
@@ -84,11 +81,11 @@ public class Chat {
 		this.current = current;
 	}
 
-	public List<_Utilisateur> getOther() {
+	public List<_Utilisateur> getOthers() {
 		return others;
 	}
 
-	private void setOther(List<_Utilisateur> others) {
+	private void setOthers(List<_Utilisateur> others) {
 		this.others = others;
 	}
 
@@ -107,56 +104,72 @@ public class Chat {
 	private void setShared(_Shared shared) {
 		this.shared = shared;
 	}
-	
+
 	/**
 	 * Lance le thread d'ecoute du chat
-	 * @throws RemoteException leve une exception en cas d'echec de communication
+	 * 
+	 * @throws RemoteException
+	 *             leve une exception en cas d'echec de communication
 	 */
 	public void listener() throws RemoteException {
-		
-		if(listenerThread == null)
-			listenerThread = new ListenerThread(this.getCurrent(), this.getShared());
-		if(notifs == null)
-			notifs = new NotificationThread(this.getShared(), this.getCurrent());
-		
-		listenerThread.start();
-		notifs.start();
+
+		// if(listener == null)
+		// listener = new ListenerThread(this.getCurrent(), this.getShared());
+		// if(notifs == null)
+		// notifs = new NotificationThread(this.getShared(), this.getCurrent());
+
+		new ListenerThread(this.getCurrent(), this.getShared()).start();
+		new NotificationThread(this.getShared(), this.getCurrent()).start();
+		// listenerThread.start();
+		// notifs.start();
 	}
-	
+
 	public void stopListener() {
-		if(listenerThread != null)
-			listenerThread.stopListener();
+		// if(listener != null)
+		// listener.stopListener();
 	}
+
 	/**
 	 * Lance le thread d'envoi sur le chat
-	 * @param escape String : chaine de caractere mettant fin a la communication
-	 * @throws RemoteException leve une exception en cas d'echec de communication
+	 * 
+	 * @param escape
+	 *            String : chaine de caractere mettant fin a la communication
+	 * @throws RemoteException
+	 *             leve une exception en cas d'echec de communication
 	 */
 	public void sender(Cmd escape) throws RemoteException {
-		
+
 		SenderThread sender = new SenderThread(this.getCurrent(), this.getShared(), escape);
 		sender.start();
 	}
-	
+
 	/**
 	 * Envoi l'objet rObject sur le chat
-	 * @param rObject _RemotableObject : objet a envoyer sur le chat
-	 * @throws RemoteException leve une exception en cas d'echec de communication
-	 * @throws CommunicationException leve une exception en cas d'acces refuse
+	 * 
+	 * @param rObject
+	 *            _RemotableObject : objet a envoyer sur le chat
+	 * @throws RemoteException
+	 *             leve une exception en cas d'echec de communication
+	 * @throws CommunicationException
+	 *             leve une exception en cas d'acces refuse
 	 */
 	public void send(_RemotableObject rObject) throws RemoteException, CommunicationException {
 		this.getCurrent().send(this.getShared(), rObject);
 	}
-	
+
 	/**
 	 * Envoi le message sur le chat
-	 * @param message String : message a envoyer
-	 * @throws RemoteException leve une exception en cas d'echec de communication
-	 * @throws CommunicationException leve une exception en cas d'acces refuse
+	 * 
+	 * @param message
+	 *            String : message a envoyer
+	 * @throws RemoteException
+	 *             leve une exception en cas d'echec de communication
+	 * @throws CommunicationException
+	 *             leve une exception en cas d'acces refuse
 	 */
 	public void send(String message) throws RemoteException, CommunicationException {
 		_RemotableObject msg = new Message(this.getCurrent(), message);
 		this.getCurrent().send(this.getShared(), msg);
 	}
-	
+
 }
